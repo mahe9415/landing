09ecf4252de57ad0c78ddbd54ad908aeadb58616
mahe <mahesh9415@gmail.com>
2017-11-23 12:20:04 +0530

    vue configured with router, vueitify and store

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div id="app">
+    <v-app>
+      <router-view name="header"> </router-view>
+      <router-view name="content"> </router-view>
+      <router-view />
+      </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+ 
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+   <p class="text-xs-center">{{welcomeTxt}}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed:
+    mapState({
+        welcomeTxt: state => state.welcome
+    })
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
