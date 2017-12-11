0e198914e9403ad7021ca8d1e192d996729f0fb0
mahe <mahesh9415@gmail.com>
2017-12-11 10:47:51 +0530

    medium post

--- src/components/landing.vue
+++ src/components/landing.vue
@@ -1,5 +1,5 @@
 <template>
-<v-container grid-list-md text-xs-center>
+<v-container grid-list-md text-xs-center d-flex align-center>
     <v-layout row wrap>
       <!-- <v-flex xs12> -->
       <!-- </v-flex> -->
@@ -20,17 +20,30 @@
                     ></v-card-media>
                 </v-flex>
       </v-layout>
-
       </v-card>
       </v-flex>
-      <v-flex xs12 md6>
-         <v-card>
-      <v-layout row>
-      
-      </v-layout>
+       <v-flex xs12 md6>
+   <!--    <v-card>
+        <v-card-title primary-title>
+        <h3 class="headline mb-0 text-xs-center"> Writings </h3>
+        </v-card-title>
+             <div class="posts d-flex">
+       <a class="post-link" href="https://blog.nfnlabs.in/goof-in-geeks-jungle-88c7e06e980b" target="_blank">
+        <h5 class="post-heading">Goof in geeks’ jungle</h5>
+        </a>
+      </div>
+      </v-card> -->
+       <v-list two-line>
+       <h2 class="headline">Writings</h2>
+          <v-list-tile href="https://blog.nfnlabs.in/goof-in-geeks-jungle-88c7e06e980b" target="_blank">
+            <!-- <v-list-tile-content> -->
+              <v-list-tile-title class="text-xs-center">Goof in geeks’ jungle</v-list-tile-title>
+              <!-- <v-list-tile-sub-title> </v-list-tile-sub-title> -->
+            <!-- </v-list-tile-content> -->
+          </v-list-tile>
+        </v-list>
+    </v-flex>
 
-      </v-card>
-      </v-flex>
      </v-layout>
   </v-container>
 </template>
@@ -42,6 +55,7 @@ name:'landing'
 </script>
 
 <style>
-	
-
+.post-link{
+  text-decoration: none;
+}
 </style>
