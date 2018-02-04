521f18702b81e2d5ea71afe531f5ef0cf27ed557
mahe94 <mahesh9415@gmail.com>
2018-02-04 13:34:25 +0530

    cards, hero-image, hero-txt done

--- src/components/landing.vue
+++ src/components/landing.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contain-app">
-    <div class="container rectangle">
-      
+    <div class="container rectangle centerify">
+      <div class="header-wrapper">
       <div class="header">
       <div class="brand brand-head"> Mahesh </div>
 
@@ -9,42 +9,44 @@
         Follow Me: <i>fb</i> <i>tw</i>  <i>insta</i>
       </div>
       </div>
+    </div>
 
+
+    <div class="hero-wrapper">
       <div class="hero-container">
-      <p class="hero-txt hello-txt">Hello, I’m Mahesh. I am an Front end Developer.<br>
-      I Love Pixel Perfection</p>
+      <p class="hero-txt hello-txt">Hello, I’m Mahesh. I am an Front end Developer. I Love Pixel Perfection</p>
       <div class="hero-img">
-        <img class="mahe-avatar" src="/static/mahe.jpg" alt="">
+        <img class="mahe-avatar" src="/static/mahe2X.jpg" alt="">
       </div>
     </div>
+  </div>
 
-
-
+<div class="articles-wrapper">
+    <h2 class="articles-head">Check my recent articles:</h2>
     <div class="articles">
-      <h2>Check my recent articles:</h2>
-      <div class="article">
-        <h4>Name</h4>
-        <p>
-         USPS has updated their postage rates and package options. Make sure you know how these changes affect your store.
+      <a class="article" href="https://codeburst.io/message-passing-using-window-object-b928df8077a6" target="_blank">
+        <h4>Listen to localStorage and dance </h4>
+        <p class="is-line-2">
+         Yup! We can use window object to pass message between tabs(which is yours) in a same window. Lets walk together 🚶🏻‍♂️
         </p>
-      </div>
+      </a>
 
 
-            <div class="article">
-        <h4>Name</h4>
-        <p>
-         USPS has updated their postage rates and package options. Make sure you know how these changes affect your store.
+            <a class="article" href="https://codeburst.io/vuex-store-d888de10d499" target="_blank">
+        <h4>Vuex Store</h4>
+        <p class="is-line-2">
+         Vuex is a simple state management library for Vue js. It is very easy to use. It serves as a centralized store for all the components in an application, with rules ensuring that the state can only be mutated in a predictable fashion.
         </p>
-      </div>
+      </a>
 
-            <div class="article">
-        <h4>Name</h4>
-        <p>
-         USPS has updated their postage rates and package options. Make sure you know how these changes affect your store.
+            <a class="article" href="https://medium.com/@mahesh9415/publish-chrome-extension-test-build-to-google-group-b4bc4d92d4d1" target="_blank">
+        <h4>Publish Chrome Extension Test Build To Google</h4>
+        <p class="is-line-2">
+        Ohh NOOO!!! Latest chrome update doesn’t allow manual installation of chrome extension (.crx) file.
         </p>
-      </div>
+      </a>
     </div>
-
+</div>
 
   <!-- <MyFooter twitter_handle="twitter" fb_handle="fb" google_handle="goo" /> -->
 </div></div>
@@ -61,25 +63,45 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css?family=Lato:400,700');
+
+.header-wrapper,.hero-wrapper,.articles-wrapper{
+  width:100%;
+}
+.hero-wrapper{
+  margin-top: 2rem;
+  margin: 2rem;
+}
 .articles{
   display: flex;
-
 }
 .article{
-  margin-top: 2%;
+    margin-top: 2%;
     padding:1.8%;
-  /*height: 122px;*/
-  width: 371px;
+    text-decoration: none;
+    font-family: inherit;
+    font-style: inherit;
+    height: 100%;
+    width: 100%;
   color:#4A4A4A;
   margin-left: 1%;
+  text-align: left;
   background-color: #FFFFFF;
   box-shadow: 0 0 0 1px rgba(63,63,68,0.05), 0 1px 3px 0 rgba(63,63,68,0.15);
 }
 .article:first-child{
   margin-left:0;
+  border-top: #47C1BF;
 }
 .article h4{
+  margin-top: 0;
   color:#212B36;
+font-family: 'Lato', sans-serif;
+font-weight: 700;
+  font-size: 1rem;
+}
+.article p{
+  color: #4A4A4A;
 }
 .articles h2{
   color: #FFFFFF;
@@ -103,6 +125,8 @@ export default {
   font-size: 2rem;
   font-weight: 500;
   line-height: 44px;
+  max-width: 715px;
+  text-align: left;
   /*margin: 2%;*/
 }
 .rectangle {
@@ -110,12 +134,14 @@ export default {
   /*height: 700px;*/
   /*width: 1280px;*/
   padding: 2%;
-  border: 1px solid #979797;
+  /*border: 1px solid #979797;*/
   background-color: #0D071E;
 }
 
 .contain-app{
   position: relative;
+  text-align: center;
+    font-family: 'Nunito', sans-serif;
   /*padding: 2%;*/
 }
 .header{
@@ -135,6 +161,62 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-between;
+}
+.mahe-avatar{
+  max-width: 300px;
+  /*height: 300px*/
+}
+.centerify{
+      display: flex;
+    flex-direction: column;
+    align-items: center;
+    /*justify-content: center;*/
+}
+.articles-head{
+    width: 100%;
+    text-align: left;
+}
+
+.is-line-2{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    line-height: 21px;
+    max-height: 40px;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+}
+.t-left{
+  float: left;
+}
+@media only screen 
+and (max-device-width : 667px){
+  .t-left{
+    float: none;
+  }
+.hello-txt{
+  color: red;
+  text-align: center;
+}
+.hero-container{
+  flex-direction: column-reverse;
+}
+.articles{
+  flex-direction: column;
+}
+.article{
+    margin:1rem auto;
+}
+.article:first-child{
+   margin:1rem auto;
+}
+}
+
+@media only screen 
+and (min-device-height : 900px){
+  .centerify{
+    justify-content: center;
+  }
 }
 /*.post-link{
   text-decoration: none;
