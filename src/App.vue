5e5baf56423390e915a1bd24d09e2d5cc19d7640
cb-maheshkumar <maheshkumar@chargebee.com>
2019-09-14 20:11:27 +0530

    new start

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,30 @@
 <template>
- <div id="app">
-<!--       <v-parallax src="https://vuetifyjs.com/static/doc-images/parallax/material.jpg" class="parallax"> -->
-      <router-view name="header"> </router-view>
-      <router-view default/>
-      <router-view name="footer"/>
-      <!-- </v-parallax> -->
+  <div id="app">
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css?family=Nunito:300i,400,600|Open+Sans:400,600')
 
+<style>
+@import url('https://fonts.googleapis.com/css?family=Nunito:300i,400,600|Open+Sans:400,600');
 #app {
     background: inherit;
     color: #fff;
-    /*padding: 2%;*/
-    /*font-family: 'Open Sans', sans-serif;*/
-
 }
 body {
   margin: 0;
   padding: 0;
   font-family: 'Nunito', sans-serif;
-
-	/*background: url('/static/bg.jpg');*/
 }
-/*.parallax{
-	min-height: 100vh;
-}*/
 </style>
